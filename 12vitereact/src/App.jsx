bf1e7f882a3c2a9857eab58efb9678e71c41ee87
uttpal singh <uttpalsingh8@gmail.com>
2025-12-02 01:14:29 +0530

    useEffect

---
+++ 12vitereact/src/App.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react'
+
+function App() {
+  const[num,setnum] = useState(10)
+  const[num1,setnum1] = useState(100)
+
+  useEffect(()=>{
+    console.log("sanjana");
+    
+  },[num])
+
+  return (
+    <div className='h-screen w-full bg-black text-white'>
+      <h1 className='text-6xl'>{num}</h1>
+
+      <button onClick={() =>{
+        setnum(num+1)
+      }} className='text-4xl border bg-white text-black p-2'>click</button>
+
+      <h1 className='text-6xl'>{num1}</h1>
+      <button onClick={() => {
+        setnum1(num1+100)
+      }} className='text-4xl border bg-white text-black p-2'> click</button>
+    </div>
+  )
+}
+
+export default App
