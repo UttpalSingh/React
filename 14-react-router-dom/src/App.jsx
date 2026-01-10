530703a9277c3ee53e259d6a1ebb01aff2443651
uttpal singh <uttpalsingh8@gmail.com>
2026-01-10 09:37:11 +0530

    routing

---
+++ 14-react-router-dom/src/App.jsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router-dom"
+import About from "./pages/about"
+import Contact from "./pages/Contact"
+import Home from "./pages/Home"
+  
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element= {<Home />} />
+      <Route path="/home" element= {<Home />} />
+      <Route path="/contact" element= {<Contact />} />
+    </Routes>
+  )
+}
+
+export default App
